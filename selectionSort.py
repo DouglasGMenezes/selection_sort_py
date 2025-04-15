ed242cdcfa131a16ff3b659c8e2bbc55340ae428
Douglas Menezes <douglasgm@MacBook-Air-de-Douglas.local>
2025-04-14 21:09:56 -0300

    add selection sort

---
+++ selectionSort.py
@@ -0,0 +1,164 @@
+# # SELECTION SORT
+#
+#
+
+# class SelectionSort:
+#     def __init__(self):
+#         self.inicio = None
+#
+#     def inserir(self, pessoa):
+#         novo_nodo = Nodo(pessoa)
+#         if self.inicio is None:
+#             self.inicio = novo_nodo
+#         else:
+#             atual = self.inicio
+#             while atual.proximo is not None:
+#                 atual = atual.proximo
+#             atual.proximo = novo_nodo
+#
+#     def exibir(self):
+#         atual = self.inicio
+#         while atual is not None:
+#             print(atual.pessoa)
+#             atual = atual.proximo
+#
+#     def ordenar_por_idade(self):
+#         atual = self.inicio
+#         while atual is not None:
+#             menor = atual
+#             proximo = atual.proximo
+#             while proximo is not None:
+#                 if proximo.pessoa.idade < menor.pessoa.idade:
+#                     menor = proximo
+#                 proximo = proximo.proximo
+#             # Troca os dados (sem mexer nos ponteiros)
+#             atual.pessoa, menor.pessoa = menor.pessoa, atual.pessoa
+#             atual = atual.proximo
+#
+#
+# if __name__ == "__main__":
+#     lista = SelectionSort()
+#
+#     lista.inserir(Pessoa("Alice", 30, "111.111.111-11"))
+#     lista.inserir(Pessoa("Bruno", 25, "222.222.222-22"))
+#     lista.inserir(Pessoa("Carla", 28, "333.333.333-33"))
+#
+#     print("Antes da ordenação:")
+#     lista.exibir()
+#
+#     lista.ordenar_por_idade()
+#
+#     print("\nDepois da ordenação por idade:")
+#     lista.exibir()
+
+
+
+# def selection_sort(list):
+#
+#     if not list:
+#         return list
+#     for i in range(len(list)):
+#         min_i = i
+#         for j in range(i + 1, len(list)):
+#             if list[j] < list[min_i]:
+#                 min_i = j
+#         list[i] , list[min_i] = list[min_i] , list[i]
+#
+#     print(list)
+#
+# selection_sort([3,12,2,14,4,5,6,67,60])
+
+
+class Pessoa:
+    def __init__(self, nome, idade, cpf):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+
+    def __str__(self):
+        return f"{self.nome} - {self.idade} anos - CPF: {self.cpf}"
+
+###############################
+
+class Nodo:
+    def __init__(self, pessoa):
+        self.pessoa = pessoa
+        self.proximo = None
+
+###############################
+
+
+class SelectionSort:
+    def __init__(self):
+        self.inicio = None
+
+    def inserir(self, pessoa):
+        novo = Nodo(pessoa)
+        if not self.inicio:
+            self.inicio = novo
+        else:
+            atual = self.inicio
+            while atual.proximo:
+                atual = atual.proximo
+            atual.proximo = novo
+
+    def exibir(self):
+        atual = self.inicio
+        while atual:
+            print(atual.pessoa)
+            atual = atual.proximo
+
+    def contar_nodos(self):
+        count = 0
+        atual = self.inicio
+        while atual:
+            count += 1
+            atual = atual.proximo
+        return count
+
+    def get_nodo_por_indice(self, index):
+        atual = self.inicio
+
+        for _ in range(index):
+            if atual is None:
+                return None
+            atual = atual.proximo
+        return atual
+
+
+    def ordenar_por_idade(self):
+        tamanho = self.contar_nodos()
+
+        for i in range(tamanho):
+            min_index = i
+            nodo_min = self.get_nodo_por_indice(i)
+
+            for j in range(i + 1, tamanho):
+                nodo_j = self.get_nodo_por_indice(j)
+                if nodo_j.pessoa.idade < nodo_min.pessoa.idade:
+                    nodo_min = nodo_j
+                    min_index = j
+
+            nodo_i = self.get_nodo_por_indice(i)
+            nodo_i.pessoa, nodo_min.pessoa = nodo_min.pessoa, nodo_i.pessoa
+
+
+
+if __name__ == "__main__":
+    lista = SelectionSort()
+
+    lista.inserir(Pessoa("Alice", 30, "111.111.111-11"))
+    lista.inserir(Pessoa("Bruno", 25, "222.222.222-22"))
+    lista.inserir(Pessoa("Carla", 28, "333.333.333-33"))
+    lista.inserir(Pessoa("Joao", 18, "444.444.444-44"))
+
+    print("Antes:")
+    lista.exibir()
+
+    lista.ordenar_por_idade()
+
+    print("\nDepois por idade:")
+    lista.exibir()
+
+
+
